d3eaa6791eac821ce8758b4e34135f99b56bcac0
tomghaines <tomhaines001@gmail.com>
2024-08-15 15:37:49 +0100

    checksum.go algorithm added

--- pkg/tle/checksum.go
+++ pkg/tle/checksum.go
@@ -1,5 +1,33 @@
 package tle
 
-func TLELineChecksum(str string) {
+import (
+	"unicode"
+)
+
+// Function to compute TLE line checksum & confirm TLE validity
+
+/*
+Compute the checksum of the line `str` modulo 10.
+
+The algorithm is simple: add all the numbers in the line, ignoring letters, spaces, periods,
+and plus signs, but assigning +1 to the minus signs. The checksum is the remainder of the
+division by 10.
+*/
+
+func TLELineChecksum(str string) int {
+	checksum := 0
+
+	for _, c := range str {
+		// Check if `c` is a digit or a minus sign
+		if unicode.IsDigit(c) {
+			digit := int(c - '0')
+			checksum += digit
+		} else if c == '-' {
+			checksum += 1
+		}
+	}
+
+	// Return checksum modulo 10
+	return checksum % 10
 
 }
