9e2b8870796b598bcd691ed07c5c6ccaf5613dd3
tomghaines <tomhaines001@gmail.com>
2024-08-16 12:03:18 +0100

    altered for loop in checksum, some tests still failing

--- pkg/tle/checksum.go
+++ pkg/tle/checksum.go
@@ -4,12 +4,21 @@ import (
 	"unicode"
 )
 
-// Compute TLE line checksum & confirm TLE validity
+// Function to compute TLE line checksum & confirm TLE validity
+
+/*
+Compute the checksum of the line `str` modulo 10.
+
+The algorithm is simple: add all the numbers in the line, ignoring letters, spaces, periods,
+and plus signs, but assigning +1 to the minus signs. The checksum is the remainder of the
+division by 10.
+*/
+
 func TLELineChecksum(str string) int {
 	checksum := 0
 
 	// Ensure we only process the first 68 characters
-	for i, c := range str[:68] {
+	for _, c := range str[:68] {
 		// Check if `c` is a digit or a minus sign
 		if unicode.IsDigit(c) {
 			digit := int(c - '0')
